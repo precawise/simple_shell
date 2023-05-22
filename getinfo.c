350a8e1e9896afac80ea1ad547bdd82c96106b65
ymautia <ymautia@gmail.com>
2023-05-22 03:30:14 +0300

    update

---
+++ getinfo.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * clear_info - initializes info_t struct
+ * @info: struct address
+ */
+void clear_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
+
+/**
+ * set_info - initializes info_t struct
+ * @info: struct address
+ * @av: argument vector
+ */
+void set_info(info_t *info, char **av)
+{
+	int index = 0;
+
+	info->filename = arguments[0];
+
+	if (info->argument)
+	{
+		info->argv = splitString(info->argument, " \t");
+		if (!info->argv)
+		{
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = duplicateString(info->argument);
+				info->argv[1] = NULL;
+			}
+		}
+		for (index = 0; info->argv && info->argv[index]; index++)
+			;
+
+		info->argc = index;
+
+		replaceAlias(info);
+		replaceVariables(info);
+	}
+}
+
+/**
+ * free_info - frees info_t struct fields
+ * @info: struct address
+ * @all: true if freeing all fields
+ */
+void free_info(info_t *info, int all)
+{
+	ffree(info->argv);
+	info->argv = NULL;
+	info->path = NULL;
+	if (all)
+	{
+		if (!info->cmd_buf)
+			free(info->arg);
+		if (info->env)
+			free_list(&(info->env));
+		if (info->history)
+			free_list(&(info->history));
+		if (info->alias)
+			free_list(&(info->alias));
+		ffree(info->environ);
+		info->environ = NULL;
+		bfree((void **)info->cmd_buf);
+		if (info->readfd > 2)
+			close(info->readfd);
+		_putchar(BUF_FLUSH);
+	}
+}
